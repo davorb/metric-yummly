ffa564a1c601aeb3c2ea2e50b5319273ad2a095f
Davor Babic <davor@davor.se>
2014-07-06 11:44:44 +0200

    First commit

---
+++ metric-yummly.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name            Metric Yummly
+// @author          Davor Babic
+// @namespace       http://www.github.com/davorb/metric-yummly
+// @description     Automatically switches from imperial to metric measurements
+// @license         Creative Commons Attribution License
+// @version         0.1
+// @include         http://www.yummly.com/*
+// @released        2014-06-06
+// @updated         2006-04-06
+// @compatible      Greasemonkey
+// ==/UserScript==
+
+/*
+ * This file is a Greasemonkey user script. To install it, you need
+ * the Firefox plugin "Greasemonkey" (URL: http://greasemonkey.mozdev.org/)
+ * After you installed the extension, restart Firefox and revisit
+ * this script. Now you will see a new menu item "Install User Script"
+ * in your tools menu.
+ *
+ * To uninstall this script, go to your "Tools" menu and select
+ * "Manage User Scripts", then select this script from the list
+ * and click uninstall :-)
+ *
+ * Creative Commons Attribution License (--> or Public Domain)
+ * http://creativecommons.org/licenses/by/2.5/
+*/
+
+(function(){
+  console.log('davor!');
+})();
